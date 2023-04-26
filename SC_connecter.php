68fa061a965994853afa08740e966897188c4a72
bilallyy <128432862+bilallyy@users.noreply.github.com>
2023-04-26 20:23:32 +0200

    Update SC_connecter.php

--- SC_connecter.php
+++ SC_connecter.php
@@ -1,82 +1,83 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
     <title>Se Connecter</title>
     <style>
 
-        /*Carte*/
-        .container {
-            width: 30%;
-            height: 58%;
-            padding: 20px;
-            background-color: rgb(111, 190, 62,0.9);
-            border-radius: 30px;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.7);
-            position: absolute;
-            top: 10%;
-            left: 34%;
-        }
+/*Carte*/
+.container {
+    width: 30%;
+    height: 58%;
+    padding: 20px;
+    background-color: rgb(111, 190, 62,0.9);
+    border-radius: 30px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.7);
+    position: absolute;
+    top: 10%;
+    left: 34%;
+}
 
-        body{
-            background-image: url("IM_background.png");
-            background-attachment: fixed;
-            background-size: 1000px;
-            text-align: center;
-            color: rgb(49, 31, 31);
-        }
+body{
+    background-image: url("IM_background.png");
+    background-attachment: fixed;
+    background-size: 1000px;
+    text-align: center;
+    color: rgb(49, 31, 31);
+}
 
-        h1 {
-            font-family: Georgia, 'Times New Roman', Times, serif;
-            font-size: 45px;
-        }
-        
-        label{
-            display: block;
-        }
+h1 {
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    font-size: 45px;
+}
 
-        input[type="text"], input[type="password"] {
-            width:  60%;
-            padding: 10px;
-            border-radius: 5px;
-            border: none;
-            margin-bottom: 40px;
-            box-shadow: 0 0 15px rgba(0, 0, 0, 0.4);
-        }
-        
-        input[type="submit"] {
-            margin: auto;
-            background-color: #237726;
-            color: #fff;
-            border: none;
-            padding: 15px;
-            border-radius: 15px;
-            cursor: pointer;
-            display: block;
-            font-weight: bold;
-        }
-        input[type="submit"]:hover {
-            background-color: #3f2377;
-        }
+label{
+    display: block;
+}
 
-        .inscription {
-            background-color: #237726;
-            color: #fff;
-            text-decoration: none;
-            padding: 15px;
-            border-radius: 15px;
-            font-weight: bold;
-        }
-        .inscription:hover {
-            background-color: #3f2377;
-        }
+input[type="text"], input[type="password"] {
+    width:  60%;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    margin-bottom: 40px;
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.4);
+}
 
-    </style>
+input[type="submit"] {
+    margin: auto;
+    background-color: #237726;
+    color: #fff;
+    border: none;
+    padding: 15px;
+    border-radius: 15px;
+    cursor: pointer;
+    display: block;
+    font-weight: bold;
+}
+input[type="submit"]:hover {
+    background-color: #3f2377;
+}
+
+.inscription {
+    background-color: #237726;
+    color: #fff;
+    text-decoration: none;
+    padding: 15px;
+    border-radius: 15px;
+    font-weight: bold;
+}
+.inscription:hover {
+    background-color: #3f2377;
+}
+
+</style>
+
 </head>
 
 <body>
     <div class="container">
         <h1>CONNEXION</h1>
-        <form action="login_check.php" method="post" autocomplete="off">
+        <form action="login_check.php" method="post">
             <label for="username">Identifiant:</label>
             <input type="text" id="username" name="username" required>
 
@@ -85,6 +86,9 @@
 
             <input type="submit" value="Se Connecter">
         </form>
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
         <p>*Je n'ai pas encore de compte*</p>
         <a href="inscription1.php" class="inscription">S'inscrire</a>
     </div>
